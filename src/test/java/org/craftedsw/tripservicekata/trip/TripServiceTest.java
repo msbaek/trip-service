0f749df355f1acd9c4f05c9691133e8cab8fb494
Myeongseok Baek <msbaek@daumcorp.com>
2013-05-15 16:54:09 +0900

    B-rector hidden logged in user

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -5,17 +5,22 @@ import org.craftedsw.tripservicekata.user.User;
 import org.junit.Test;
 
 public class TripServiceTest {
+    private User loggedInUser;
+
     @Test(expected = UserNotLoggedInException.class)
     public void
     should_throw_an_exception_when_user_is_not_logged_in() throws Exception {
         TripService tripService = new TestingTripService();
+
+        loggedInUser = null;
+
         tripService.getTripsByUser(null);
     }
 
     class TestingTripService extends TripService {
         @Override
         User getLoggedUser() {
-            return null;
+            return loggedInUser;
         }
     }
 }
