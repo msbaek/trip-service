37cf98d5ff1d4b26a40102f772bb2c546e3a6728
Myeongseok Baek <msbaek@daumcorp.com>
2013-05-15 16:29:54 +0900

    initial commit

---
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -0,0 +1,30 @@
+package org.craftedsw.tripservicekata.trip;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.craftedsw.tripservicekata.user.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripService {
+    public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
+        List<Trip> tripList = new ArrayList<Trip>();
+        User loggedUser = UserSession.getInstance().getLoggedUser();
+        boolean isFriend = false;
+        if (loggedUser != null) {
+            for (User friend : user.getFriends()) {
+                if (friend.equals(loggedUser)) {
+                    isFriend = true;
+                    break;
+                }
+            }
+            if (isFriend) {
+                tripList = TripDAO.findTripsByUser(user);
+            }
+            return tripList;
+        } else {
+            throw new UserNotLoggedInException();
+        }
+    }
+}
