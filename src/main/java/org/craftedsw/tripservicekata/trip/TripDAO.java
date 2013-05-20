f8a857ad0485bd92818ad76fb64581cde06e794e
Myeongseok Baek <msbaek@daumcorp.com>
2013-05-20 10:01:52 +0900

    static method 제거
    - 원하는 바를 테스트에 기술

--- src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
+++ src/main/java/org/craftedsw/tripservicekata/trip/TripDAO.java
@@ -1,9 +1,9 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.DependendClassCallDuringUnitTestException;
 import org.craftedsw.tripservicekata.user.User;
+
+import java.util.List;
 
 public class TripDAO {
 
@@ -11,5 +11,8 @@ public class TripDAO {
 		throw new DependendClassCallDuringUnitTestException(
 				"TripDAO should not be invoked on an unit test.");
 	}
-	
+
+    public List<Trip> tripsBy(User user) {
+        return TripDAO.findTripsByUser(user);
+    }
 }
